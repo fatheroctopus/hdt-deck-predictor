381650e9fc305e22ed2398bd69bc628985179e06
Geoff Pitsch <geoff.pitsch@gmail.com>
2018-02-18 21:53:52 -0500

    Empty plugin copied from PluginExample

---
+++ DeckPredictor/DeckPredictorPlugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace DeckPredictor
+{
+	public class DeckPredictorPlugin : IPlugin
+	{
+		public string Author
+		{
+			get { return "gpitsch"; }
+		}
+
+		public string ButtonText
+		{
+			get { return "Settings"; }
+		}
+
+		public string Description
+		{
+			get { return "A plugin that will predict the contents of the opponent's deck."; }
+		}
+
+		public MenuItem MenuItem
+		{
+			get { return null; }
+		}
+
+		public string Name
+		{
+			get { return "Deck Predictor"; }
+		}
+
+		public void OnButtonPress()
+		{
+		}
+
+		public void OnLoad()
+		{
+		}
+
+		public void OnUnload()
+		{
+		}
+
+		public void OnUpdate()
+		{
+		}
+
+		public Version Version
+		{
+			get { return new Version(0, 1, 1); }
+		}
+	}
+}
