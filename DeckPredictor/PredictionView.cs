547057abf141e8a3fa72b2f42f72259a72e4e7df
Geoff Pitsch <geoff.pitsch@gmail.com>
2018-03-07 19:18:41 -0500

    Disable HDT opponent card list when PredictionView is up

--- DeckPredictor/PredictionView.cs
+++ DeckPredictor/PredictionView.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker;
 using Core = Hearthstone_Deck_Tracker.API.Core;
 
 namespace DeckPredictor
@@ -14,9 +15,9 @@ namespace DeckPredictor
 	public class PredictionView
 	{
 		private PredictionLayout _layout = new PredictionLayout();
+		private bool _lastHideOpponentCards;
 
-		public PredictionView() {
-		}
+		public PredictionView() {}
 
 		public void SetEnabled(bool enabled)
 		{
@@ -27,11 +28,16 @@ namespace DeckPredictor
 				Canvas.SetBottom(_layout, Core.OverlayWindow.Height * 20 / 100);
 				Canvas.SetLeft(_layout, Core.OverlayWindow.Width * .5 / 100);
 				_layout.Dispatcher.Invoke(() => _layout.Visibility = Visibility.Hidden);
+
+				// Turn off the regular Opponent card list and restore the value later.
+				_lastHideOpponentCards = Config.Instance.HideOpponentCards;
+				Config.Instance.HideOpponentCards = true;
 			}
 			else
 			{
 				Log.Debug("Removing List View from OverlayCanvas");
 				Core.OverlayCanvas.Children.Remove(_layout);
+				Config.Instance.HideOpponentCards = _lastHideOpponentCards;
 			}
 		}
 
